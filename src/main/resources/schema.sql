406843ef527ad1c884694e4c4a4e3e1a93629aae
Amos Feng <zfeng@redhat.com>
2019-03-18 22:37:50 +0800

    work for ms sqlserver

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,5 @@
-create table if not exists audit_log (
-  audit_id serial primary key,
-  message varchar(255) not null
+IF NOT EXISTS (  SELECT [name] FROM sys.tables WHERE [name] = 'audit_log')
+create table audit_log (
+  audit_id     INT           NOT NULL    IDENTITY    PRIMARY KEY,
+  message      VARCHAR(50)   NOT NULL
 );
